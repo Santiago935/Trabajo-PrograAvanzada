1824e66b23dc841d76d13901216d060abbdc0fa0
GonzaloGhiano <gonzaghiano@hotmail.com>
2025-06-09 00:03:45 -0300

    Agrego el workspace del proyecto

---
+++ Progra_TP2/.metadata/.plugins/org.eclipse.core.resources/.history/1e/40758080294200101756e9bfe40568e7
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module tp_robots_programacion_avanzada {
+}
---
+++ Progra_TP2/tp_robots_programacion_avanzada/src/grafos/Nodo.java
@@ -0,0 +1,29 @@
+package grafos;
+
+public class Nodo {
+	private final int id;
+	private String alias = null;
+	
+	public Nodo(int id)
+	{
+		this.id = id;
+	}
+	
+	public Nodo(int id, String alias)
+	{
+		this.id = id;
+		this.alias = alias;
+	}
+	
+    public int getId() {
+        return id;
+    }
+    
+    @Override
+    public String toString() {
+        if (alias == null) {
+            return "Nodo(" + id + ")";
+        }
+        return "Nod(" + id + ": " + alias + ")";
+    }
+}
