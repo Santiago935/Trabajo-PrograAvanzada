0389d4288611c0660dd92b7cb3334fd7fbb87c1a
Rodrigo Ramato <134215749+dragonamatista@users.noreply.github.com>
2025-06-20 21:03:00 -0300

    Delete Trabajo-PrograAvanzada-main directory

--- Trabajo-PrograAvanzada-main/Progra_TP2/tp_robots_programacion_avanzada/src/cofres/PuedeOfertar.java
+++
@@ -1,9 +0,0 @@
-package cofres;
-
-import utiles.Item;
-
-public interface PuedeOfertar {
-	int cuantoOfrece(Item item); // devuelve cuánto stock tiene para ofrecer del item
-
-	boolean reservarItem(Item item, int cantidad); // intenta reservar item, devuelve si pudo
-}
--- Trabajo-PrograAvanzada-main/Progra_TP2/tp_robots_programacion_avanzada/src/cofres/PuedeSolicitar.java
+++
@@ -1,9 +0,0 @@
-package cofres;
-
-import utiles.Item;
-
-public interface PuedeSolicitar {
-	int cuantoSolicita(Item item); // devuelve la cantidad solicitada de un item
-
-	void aceptarPedido(Item item, int cantidad); // notifica que se le entrego parte del pedido
-}
